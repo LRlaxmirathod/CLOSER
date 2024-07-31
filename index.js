25558ebf539a57b111882f34a9fbd924095ab696
LRlaxmirathod <168898815+LRlaxmirathod@users.noreply.github.com>
2024-07-31 14:04:20 +0530

    CLOSER
    HI I AM CLOSER I HIDE THE DATA

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function iamcloserihidethedata(){
+    let count = 0;
+    document.querySelector("button").addEventListener('click' , ()=>{
+        console.log("button was clicked" , ++count)
+    })
+}
+
+iamcloserihidethedata()
+
